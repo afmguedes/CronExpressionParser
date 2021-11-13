ee395eb8a6964f2a4cee47f06b8b35421f306027
Andre Guedes <afm.guedes@hotmail.com>
2021-11-13 21:55:32 +0000

    Add ability to parse range and increments from star

--- src/CronExpressionParser.Core/Fields/MinutesField.cs
+++ src/CronExpressionParser.Core/Fields/MinutesField.cs
@@ -10,14 +10,41 @@ namespace CronExpressionParser.Core.Fields
 
 		public List<int> TryParse(string minutesExpression)
 		{
-			var values = minutesExpression.Split(',');
+			var valuesSplitByComma = minutesExpression.Split(',');
 
-			if (values.Length > 1)
+			if (valuesSplitByComma.Length > 1)
 			{
-				return values.Select(int.Parse).ToList();
+				return valuesSplitByComma.Select(int.Parse).ToList();
 			}
 
-			return minutesExpression.Equals("*") ? new List<int>(Enumerable.Range(MINUTES_MINVALUE, MINUTES_MAXVALUE)) : new List<int> { int.Parse(minutesExpression) };
+			var valuesSplitByDash = minutesExpression.Split('-');
+
+			if (valuesSplitByDash.Length > 1)
+			{
+				return new List<int>(Enumerable.Range(int.Parse(valuesSplitByDash[0]),
+					int.Parse(valuesSplitByDash[1])));
+			}
+
+			var valuesSplitBySlash = minutesExpression.Split('/');
+
+			if (valuesSplitBySlash.Length > 1)
+			{
+				return new List<int>(GetIncrementsOfStartingAt(int.Parse(valuesSplitBySlash[1]), 0));
+			}
+
+			return minutesExpression.Equals("*") ? new List<int>(Enumerable.Range(MINUTES_MINVALUE, MINUTES_MAXVALUE)) : new List<int> { int.Parse(valuesSplitByComma.First()) };
+		}
+
+		private static IEnumerable<int> GetIncrementsOfStartingAt(int increment, int start)
+		{
+			var incrementedValues = new List<int>();
+
+			for (var i = start; i <= MINUTES_MAXVALUE; i += increment)
+			{
+				incrementedValues.Add(i);
+			}
+
+			return incrementedValues;
 		}
 	}
 }
--- src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
+++ src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
@@ -12,7 +12,9 @@ namespace CronExpressionParser.UnitTests
 		{
 			new TestCaseData("1", new List<int> { 1 }).SetName("SingleValueExpression"),
 			new TestCaseData("*", new List<int>(Enumerable.Range(0, 59))).SetName("SingleStarExpression"),
-			new TestCaseData("1, 2, 3", new List<int> { 1, 2, 3 }).SetName("AdditionalValuesExpression")
+			new TestCaseData("1, 2, 3", new List<int> { 1, 2, 3 }).SetName("AdditionalValuesExpression"),
+			new TestCaseData("1-5", new List<int> { 1, 2, 3, 4, 5 }).SetName("RangeExpression"),
+			new TestCaseData("*/10", new List<int> { 0, 10, 20, 30, 40, 50 }).SetName("IncrementsFromStarExpression")
 		};
 
 		[TestCaseSource(nameof(TestCases))]
