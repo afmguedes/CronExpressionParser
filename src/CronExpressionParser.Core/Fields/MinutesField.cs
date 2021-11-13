680b72e478d3878baa7ddc7d410f6d9897784110
Andre Guedes <afm.guedes@hotmail.com>
2021-11-13 23:37:33 +0000

    Add unhappy path tests; Refactor logic

--- src/CronExpressionParser.Core/Fields/MinutesField.cs
+++ src/CronExpressionParser.Core/Fields/MinutesField.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CronExpressionParser.Core.Fields
@@ -10,32 +11,55 @@ namespace CronExpressionParser.Core.Fields
 
 		public List<int> TryParse(string minutesExpression)
 		{
+			if (minutesExpression.Equals("*"))
+			{
+				minutesExpression = $"{MINUTES_MINVALUE}-{MINUTES_MAXVALUE}";
+			}
+
 			var valuesSplitByComma = minutesExpression.Split(',');
 
 			if (valuesSplitByComma.Length > 1)
 			{
-				return valuesSplitByComma.Select(int.Parse).ToList();
+				return TryParseIntegers(valuesSplitByComma).ToList();
 			}
 
 			var valuesSplitByDash = minutesExpression.Split('-');
 
 			if (valuesSplitByDash.Length > 1)
 			{
-				return new List<int>(Enumerable.Range(int.Parse(valuesSplitByDash[0]),
-					int.Parse(valuesSplitByDash[1])));
+				var parsedIntegersSplitByDash = TryParseIntegers(valuesSplitByDash).ToList();
+				return new List<int>(Enumerable.Range(parsedIntegersSplitByDash[0], parsedIntegersSplitByDash[1]));
 			}
 
 			var valuesSplitBySlash = minutesExpression.Split('/');
 
 			if (valuesSplitBySlash.Length == 1)
 			{
-				return minutesExpression.Equals("*")
-					? new List<int>(Enumerable.Range(MINUTES_MINVALUE, MINUTES_MAXVALUE))
-					: new List<int> { int.Parse(minutesExpression) };
+				var parsedInteger = TryParseIntegers(new[] { minutesExpression });
+				return parsedInteger.ToList();
 			}
-				
-			var start = valuesSplitBySlash.First().Replace('*', '0');
-			return new List<int>(GetIncrementsOfStartingAt(int.Parse(valuesSplitBySlash[1]), int.Parse(start)));
+
+			valuesSplitBySlash[0] = valuesSplitBySlash[0].Replace('*', '0');
+			var parsedIntegersSplitBySlash = TryParseIntegers(valuesSplitBySlash).ToList();
+
+			return new List<int>(GetIncrementsOfStartingAt(parsedIntegersSplitBySlash[1], parsedIntegersSplitBySlash[0]));
+		}
+
+		private static IEnumerable<int> TryParseIntegers(IEnumerable<string> values)
+		{
+			var parsedIntegers = new List<int>();
+
+			foreach (var value in values)
+			{
+				if (!int.TryParse(value, out var parsedValue))
+				{
+					throw new ArgumentException($"'{value}' is not a valid value for {nameof(MinutesField)}");
+				}
+
+				parsedIntegers.Add(parsedValue);
+			}
+
+			return parsedIntegers;
 		}
 
 		private static IEnumerable<int> GetIncrementsOfStartingAt(int increment, int start)
--- src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
+++ src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CronExpressionParser.Core.Fields;
 using FluentAssertions;
@@ -8,7 +9,7 @@ namespace CronExpressionParser.UnitTests
 {
 	public class MinutesFieldShould
 	{
-		private static readonly TestCaseData[] TestCases =
+		private static readonly TestCaseData[] HappyPathTestCases =
 		{
 			new TestCaseData("1", new List<int> { 1 }).SetName("SingleValueExpression"),
 			new TestCaseData("*", new List<int>(Enumerable.Range(0, 59))).SetName("SingleStarExpression"),
@@ -18,12 +19,31 @@ namespace CronExpressionParser.UnitTests
 			new TestCaseData("5/20", new List<int> { 5, 25, 45 }).SetName("IncrementsFromValueExpression")
 		};
 
-		[TestCaseSource(nameof(TestCases))]
+		[TestCaseSource(nameof(HappyPathTestCases))]
 		public void ReturnExpectedMinutes_WhenPassed(string minutesExpression, List<int> expectedMinutes)
 		{
 			var actualMinutes = new MinutesField().TryParse(minutesExpression);
 			
 			actualMinutes.Should().BeEquivalentTo(expectedMinutes);
+		}
+
+		private static readonly TestCaseData[] UnhappyPathTestCases =
+		{
+			new TestCaseData("$", "'$' is not a valid value for MinutesField").SetName("InvalidSingleCharacterExpression"),
+			new TestCaseData("1,", "'' is not a valid value for MinutesField").SetName("InvalidAdditionalValuesExpression"),
+			new TestCaseData("2,3,=", "'=' is not a valid value for MinutesField").SetName("InvalidCharacterAdditionalValuesExpression"),
+			new TestCaseData("10-", "'' is not a valid value for MinutesField").SetName("InvalidRangeValuesExpression"),
+			new TestCaseData("0-+", "'+' is not a valid value for MinutesField").SetName("InvalidCharacterRangeValuesExpression"),
+			new TestCaseData("0/&", "'&' is not a valid value for MinutesField").SetName("InvalidCharacterIncrementValuesExpression"),
+			new TestCaseData("*/*", "'*' is not a valid value for MinutesField").SetName("InvalidCharactersIncrementValuesExpression"),
+		};
+
+		[TestCaseSource(nameof(UnhappyPathTestCases))]
+		public void ThrowArgumentException_WhenPassed(string minutesExpression, string expectedMessage)
+		{
+			Action action = () => new MinutesField().TryParse(minutesExpression);
+
+			action.Should().Throw<ArgumentException>().WithMessage(expectedMessage);
 		}
 	}
 }
