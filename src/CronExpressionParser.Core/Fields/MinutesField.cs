b839219a3b992e6e293771fdb374dbfc82810ed9
Andre Guedes <afm.guedes@hotmail.com>
2021-11-13 21:18:49 +0000

    Add ability to parse single star and additional values separated by comma

--- src/CronExpressionParser.Core/Fields/MinutesField.cs
+++ src/CronExpressionParser.Core/Fields/MinutesField.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CronExpressionParser.Core.Fields
 {
 	public class MinutesField : IField
 	{
+		private const int MINUTES_MINVALUE = 0;
+		private const int MINUTES_MAXVALUE = 59;
 
 		public List<int> TryParse(string minutesExpression)
 		{
-			return new List<int> { int.Parse(minutesExpression) };
+			var values = minutesExpression.Split(',');
+
+			if (values.Length > 1)
+			{
+				return values.Select(int.Parse).ToList();
+			}
+
+			return minutesExpression.Equals("*") ? new List<int>(Enumerable.Range(MINUTES_MINVALUE, MINUTES_MAXVALUE)) : new List<int> { int.Parse(minutesExpression) };
 		}
 	}
 }
--- src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
+++ src/CronExpressionParser.UnitTests/MinutesFieldShould.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using CronExpressionParser.Core.Fields;
 using FluentAssertions;
 using NUnit.Framework;
@@ -15,6 +16,28 @@ namespace CronExpressionParser.UnitTests
 
 			var actualMinutes = new MinutesField().TryParse(minutesExpression);
 			
+			actualMinutes.Should().BeEquivalentTo(expectedMinutes);
+		}
+
+		[Test]
+		public void ReturnRangeOfValues_WhenSingleStarExpression()
+		{
+			const string minutesExpression = "*";
+			var expectedMinutes = new List<int>(Enumerable.Range(0, 59));
+
+			var actualMinutes = new MinutesField().TryParse(minutesExpression);
+
+			actualMinutes.Should().BeEquivalentTo(expectedMinutes);
+		}
+
+		[Test]
+		public void ReturnListOfValues_WhenAdditionalValuesExpression()
+		{
+			const string minutesExpression = "1, 2, 3";
+			var expectedMinutes = new List<int> { 1, 2, 3 };
+
+			var actualMinutes = new MinutesField().TryParse(minutesExpression);
+
 			actualMinutes.Should().BeEquivalentTo(expectedMinutes);
 		}
 	}
