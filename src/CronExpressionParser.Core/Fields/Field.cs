5670fd3eef0f839ef14df23d7c05310cc057c171
Andre Guedes <afm.guedes@hotmail.com>
2021-11-14 11:22:44 +0000

    Add new abstract class and add inheritance to MinutesField

---
+++ src/CronExpressionParser.Core/Fields/Field.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CronExpressionParser.Core.Fields
+{
+	public abstract class Field
+	{
+		private readonly int MIN_VALUE; 
+		private readonly int MAX_VALUE;
+
+		protected Field(int minValue, int maxValue)
+		{
+			MIN_VALUE = minValue;
+			MAX_VALUE = maxValue;
+		}
+
+		public List<int> TryParse(string minutesExpression)
+		{
+			if (minutesExpression.Equals("*"))
+			{
+				minutesExpression = $"{MIN_VALUE}-{MAX_VALUE}";
+			}
+
+			var valuesSplitByComma = minutesExpression.Split(',');
+
+			if (valuesSplitByComma.Length > 1)
+			{
+				return TryParseIntegers(valuesSplitByComma).ToList();
+			}
+
+			var valuesSplitByDash = minutesExpression.Split('-');
+
+			if (valuesSplitByDash.Length > 1)
+			{
+				var parsedIntegersSplitByDash = TryParseIntegers(valuesSplitByDash).ToList();
+				return new List<int>(Enumerable.Range(parsedIntegersSplitByDash[0], parsedIntegersSplitByDash[1]));
+			}
+
+			var valuesSplitBySlash = minutesExpression.Split('/');
+
+			if (valuesSplitBySlash.Length == 1)
+			{
+				var parsedInteger = TryParseIntegers(new[] { minutesExpression });
+				return parsedInteger.ToList();
+			}
+
+			valuesSplitBySlash[0] = valuesSplitBySlash[0].Replace('*', MIN_VALUE.ToString().First());
+			var parsedIntegersSplitBySlash = TryParseIntegers(valuesSplitBySlash).ToList();
+
+			return new List<int>(GetIncrementsOfStartingAt(parsedIntegersSplitBySlash[1], parsedIntegersSplitBySlash[0]));
+		}
+
+		private IEnumerable<int> TryParseIntegers(IEnumerable<string> values)
+		{
+			var parsedIntegers = new List<int>();
+
+			foreach (var value in values)
+			{
+				if (!int.TryParse(value, out var parsedValue) || parsedValue > MAX_VALUE)
+				{
+					var exceptionMessage =
+						string.Format(LogMessageFactory.InvalidValueForExpressionFieldMessageTemplate, value,
+							nameof(MinutesField));
+					throw new ArgumentException(exceptionMessage);
+				}
+
+				parsedIntegers.Add(parsedValue);
+			}
+
+			return parsedIntegers;
+		}
+
+		private IEnumerable<int> GetIncrementsOfStartingAt(int increment, int start)
+		{
+			var incrementedValues = new List<int>();
+
+			for (var i = start; i <= MAX_VALUE; i += increment)
+			{
+				incrementedValues.Add(i);
+			}
+
+			return incrementedValues;
+		}
+	}
+}
--- src/CronExpressionParser.Core/Fields/MinutesField.cs
+++ src/CronExpressionParser.Core/Fields/MinutesField.cs
@@ -1,80 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace CronExpressionParser.Core.Fields
+﻿namespace CronExpressionParser.Core.Fields
 {
-	public class MinutesField : IField
+	public class MinutesField : Field, IField
 	{
-		private const int MINUTES_MINVALUE = 0;
-		private const int MINUTES_MAXVALUE = 59;
+		private const int MIN_VALUE = 0;
+		private const int MAX_VALUE = 59;
 
-		public List<int> TryParse(string minutesExpression)
+		public MinutesField() : base(MIN_VALUE, MAX_VALUE)
 		{
-			if (minutesExpression.Equals("*"))
-			{
-				minutesExpression = $"{MINUTES_MINVALUE}-{MINUTES_MAXVALUE}";
-			}
-
-			var valuesSplitByComma = minutesExpression.Split(',');
-
-			if (valuesSplitByComma.Length > 1)
-			{
-				return TryParseIntegers(valuesSplitByComma).ToList();
-			}
-
-			var valuesSplitByDash = minutesExpression.Split('-');
-
-			if (valuesSplitByDash.Length > 1)
-			{
-				var parsedIntegersSplitByDash = TryParseIntegers(valuesSplitByDash).ToList();
-				return new List<int>(Enumerable.Range(parsedIntegersSplitByDash[0], parsedIntegersSplitByDash[1]));
-			}
-
-			var valuesSplitBySlash = minutesExpression.Split('/');
-
-			if (valuesSplitBySlash.Length == 1)
-			{
-				var parsedInteger = TryParseIntegers(new[] { minutesExpression });
-				return parsedInteger.ToList();
-			}
-
-			valuesSplitBySlash[0] = valuesSplitBySlash[0].Replace('*', MINUTES_MINVALUE.ToString().First());
-			var parsedIntegersSplitBySlash = TryParseIntegers(valuesSplitBySlash).ToList();
-
-			return new List<int>(GetIncrementsOfStartingAt(parsedIntegersSplitBySlash[1], parsedIntegersSplitBySlash[0]));
-		}
-
-		private static IEnumerable<int> TryParseIntegers(IEnumerable<string> values)
-		{
-			var parsedIntegers = new List<int>();
-
-			foreach (var value in values)
-			{
-				if (!int.TryParse(value, out var parsedValue) || parsedValue > MINUTES_MAXVALUE)
-				{
-					var exceptionMessage =
-						string.Format(LogMessageFactory.InvalidValueForExpressionFieldMessageTemplate, value,
-							nameof(MinutesField));
-					throw new ArgumentException(exceptionMessage);
-				}
-
-				parsedIntegers.Add(parsedValue);
-			}
-
-			return parsedIntegers;
-		}
-
-		private static IEnumerable<int> GetIncrementsOfStartingAt(int increment, int start)
-		{
-			var incrementedValues = new List<int>();
-
-			for (var i = start; i <= MINUTES_MAXVALUE; i += increment)
-			{
-				incrementedValues.Add(i);
-			}
-
-			return incrementedValues;
 		}
 	}
 }
