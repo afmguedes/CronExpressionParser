6128becb4b710df2b943d750369e31a3590b011d
Andre Guedes <afm.guedes@hotmail.com>
2021-11-14 01:57:49 +0000

    Implement translate to text

--- src/CronExpressionParser.Core/ModelTranslator.cs
+++ src/CronExpressionParser.Core/ModelTranslator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace CronExpressionParser.Core
 {
@@ -6,7 +7,26 @@ namespace CronExpressionParser.Core
 	{
 		public static string TranslateToText(OutputModel outputModel)
 		{
-			throw new NotImplementedException();
+			var outputText = new StringBuilder();
+
+			outputText.AppendLine(FormatLine("minute", outputModel.Minutes));
+			outputText.AppendLine(FormatLine("hour", outputModel.Hours));
+			outputText.AppendLine(FormatLine("day of month", outputModel.DaysOfMonth));
+			outputText.AppendLine(FormatLine("month", outputModel.Months));
+			outputText.AppendLine(FormatLine("day of week", outputModel.DaysOfWeek));
+			outputText.AppendLine(FormatLine("command", outputModel.Command));
+
+			return outputText.ToString();
+		}
+
+		private static string FormatLine(string title, IEnumerable<int> values)
+		{
+			return $"{title.PadRight(14, ' ')}{string.Join(' ', values)}";
+		}
+
+		private static string FormatLine(string title, string command)
+		{
+			return $"{title.PadRight(14, ' ')}{command}";
 		}
 	}
 }
---
+++ src/CronExpressionParser.UnitTests/ModelTranslatorShould.cs
@@ -0,0 +1,34 @@
+﻿using CronExpressionParser.Core;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CronExpressionParser.UnitTests
+{
+	[TestFixture]
+	public class ModelTranslatorShould
+	{
+		[Test]
+		public void ReturnExpectedResult_WhenValidOutputModelPassed()
+		{
+			const string expectedOutputText = 
+				"minute        0 15 30 45\r\n" +
+			    "hour          0\r\n" +
+				"day of month  1 15\r\n" +
+				"month         1 2 3 4 5 6 7 8 9 10 11 12\r\n" +
+				"day of week   1 2 3 4 5\r\n" +
+				"command       /usr/bin/find\r\n";
+			
+			var minutes = new[] { 0, 15, 30, 45 };
+			var hours = new[] { 0 };
+			var daysOfMonth = new[] { 1, 15 };
+			var months = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+			var daysOfWeek = new[] { 1, 2, 3, 4, 5 };
+			const string command = "/usr/bin/find";
+			var outputModel = new OutputModel(minutes, hours, daysOfMonth, months, daysOfWeek, command);
+			
+			var actualOutputText = ModelTranslator.TranslateToText(outputModel);
+			
+			actualOutputText.Should().Be(expectedOutputText);
+		}
+	}
+}
